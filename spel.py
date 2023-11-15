1dcb66a333ba759d096b51bafe25d69e3a53bd50
leo <leonard.aronsson@skola.taby.se>
2023-11-15 10:55:59 +0100

    ny monster strid

--- spel.py
+++ spel.py
@@ -1,92 +1,118 @@
 import random as rand
 
-class spelare:
+class Player:
     def __init__(self):
-        self.nivå = 1
-        self.hp = 10
-        self.styrka = 10
-        self.inventarie = []
+        self.level = 1
+        self.hp = 100
+        self.strength = 10
+        self.inventory = []
 
 class Item:
     def __init__(self):
-        self.name = rand.choice(["Svärd", "Sköld", "Pilbåge"])
-        self.styrka_bonus = rand.randint(1, 5)
+        self.name = rand.choice(["Sword", "Shield", "Bow", "Armor"])
+        self.strength_bonus = rand.randint(1, 5)
 
-def visa_stats(spelare):
-    print(f"HP: {spelare.hp}")
-    print(f"Nivå: {spelare.nivå}")
-    print(f"Styrka: {spelare.styrka}")
+player = Player()
 
-def föremål_i_kista(spelare):
-    föremål = Item()
-    spelare.inventarie.append(föremål)
-    spelare.styrka += föremål.styrka_bonus
-    
-    print(f"Du har fått ett nytt vapen: {föremål.name}!")
-    print(f"Din styrka har ökat med {föremål.styrka_bonus}")
+class Monster:
+    def __init__(self):
+        self.monster_hp = rand.randint(5, 10) * player.strength
+        self.monster_strength = rand.randint(2, 5) * player.level
+        self.monster = rand.choice(["Golem", "Goblin", "Dragon", "Wyvern"])
 
-def monster_strid():
-    monster_styrka = rand.randint(1, 10) + spelare.nivå
-    monster = rand.choice(["Golem", "Goblin"])
+def monster_battle():
     print(f"""
-
-        Du har mött en {monster}!  
-        Monsterstyrka: {monster_styrka}
+            You have encountered a {monster.monster}!
+            Monster hp: {monster.monster_hp}   
+            Monster strength: {monster.monster_strength}
 
             """)
-    spelare.hp = spelare.hp - int(monster_styrka/spelare.styrka)
-    xp = monster_styrka/spelare.nivå
-    print(f"Du har fått {xp} xp")
-    summa_xp = 0
-    summa_xp += xp
-    if summa_xp >= 10:
-        spelare.nivå += 1
-        print(f"Du har gått upp i nivå till {spelare.nivå}!")
+    while monster.monster_hp > 0:
+            print(f"""
+            1. Attack
+            2. Flee
 
-spelare = spelare()
+            """)
+            choose = input("Attack or flee?  ")
+            if choose == "1":
+                monster.monster_hp -= player.strength
+                player.hp -= monster.monster_strength
+                print(f"Monster hp: {monster.monster_hp}")
+                if monster.monster_hp <= 0:
+                    print(f"You have slain the {monster.monster}!")
+                    xp = monster.monster_strength
+                    print(f"You have recieved {xp} experience!")
+            else:
+                print("You fled!")
+                break
+    sum_xp = 0
+    sum_xp += xp
+    if sum_xp > 10:
+        print("Your level has increased!")
+        player.level += sum_xp
+        player.strength += 2
 
-while spelare.hp >= 0:        
-    print(
-            """
+monster = Monster()
 
-            Vad vill du göra?
-            1. Välj dörr
-            2. Kolla ryggsäck
-            3. Kolla stats
+def display_stats(player):
+    print(f"HP: {player.hp}")
+    print(f"Level: {player.level}")
+    print(f"Strength: {player.strength}")
+
+def item_in_chest(player):
+    item = Item()
+    player.inventory.append(item)
+    player.strength += item.strength_bonus
+    
+    print(f"You have received a new weapon: {item.name}!")
+    print(f"Your strength has increased by {item.strength_bonus}")
+
+
+def main():
+    while player.hp >= 0:        
+        print(
+                """
+
+                What do you want to do?
+                1. Choose a door
+                2. Check inventory
+                3. Display stats
+                
+                """)
             
-            """)
-        
-    val = input("Vad vill du göra? (1, 2 eller 3) ")
+        choice = input("What do you want to do? (1, 2 or 3) ")
 
-    if val == "1":
-        händelse = rand.randint(1, 3)
-        if händelse == 1:
-            monster_strid()
-            if spelare.hp <= 0:
-                print("Du död")
-        elif händelse == 2:
-            föremål_i_kista(spelare)
-            if len(spelare.inventarie) > 4:
-                try:
-                    print("Ditt inventarie är fullt! Välj något att byta ut!")
-                    for föremål in spelare.inventarie:
-                        print(f"{föremål.name} med styrka bonus {föremål.styrka_bonus}")
-                    ta_bort_index = int(input("Ta bort index: "))
-                    index = spelare.inventarie[ta_bort_index]
-                    spelare.inventarie.remove(index)
-                except:
-                    print("Välj ett index som finns i listan!")
-        elif händelse == 3:
-            fälla_skada = rand.randint(1, 4)
-            spelare.hp = spelare.hp - fälla_skada
-            print(f"Du har hamnat i en fälla och har förlorat {fälla_skada} HP!")
-            if spelare.hp <= 0:
-                print("Du är död!!!")
-    elif val == "2":
-        for föremål in spelare.inventarie:
-            print(f"{föremål.name} med styrka bonus {föremål.styrka_bonus}")
-    elif val == "3":
-        visa_stats(spelare)
-    else:
-        print("Välj 1, 2 eller 3!")
+        if choice == "1":
+            scenario = rand.randint(1, 3)
+            if scenario == 1:
+                monster_battle()
+                if player.hp <= 0:
+                    print("You died")
+            elif scenario == 2:
+                item_in_chest(player)
+                if len(player.inventory) > 4:
+                    try:
+                        print("Your inventory is full! Pick something to exchange!")
+                        for item in player.inventory:
+                            print(f"{item.name} with strength bonus {item.strength_bonus}")
+                        remove_index = int(input("Remove index: "))
+                        index = player.inventory[remove_index]
+                        player.inventory.remove(index)
+                    except:
+                        print("Choose an index within list range!")
+            elif scenario == 3:
+                trap_damage = rand.randint(1, 4)
+                player.hp = player.hp - trap_damage
+                print(f"You have been caught in a trap and lost {trap_damage} HP!")
+                if player.hp <= 0:
+                    print("You died")
+        elif choice == "2":
+            for item in player.inventory:
+                print(f"{item.name} with strength bonus {item.strength_bonus}")
+        elif choice == "3":
+            display_stats(player)
+        else:
+            print("Choose 1, 2 or 3!")
+
+main()
 
